05308feebe81929eaeaac19579f2977d72622f19
Dan Feng <d.feng@hotmail.com>
2024-08-16 10:02:03 +0000

    Allow hosting on ci and heroku

--- check_django/settings.py
+++ check_django/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-craigallend-djangocheck-r1utdq7zshu.ws.codeinstitute-ide.net', 'herokuapp.com']
+ALLOWED_HOSTS = ['.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
