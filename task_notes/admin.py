822c0798b46fd8a4ec8c65a2fbbed182b74da530
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-08-18 15:14:50 +0000

    AllAuth Plugged in and Signin added to Base.html

--- task_notes/admin.py
+++ task_notes/admin.py
@@ -4,7 +4,7 @@ from .models import Notes
 # Register your models here.
 @admin.register(Notes)
 class NotesAdmin(admin.ModelAdmin):
-    list_display = ('title', 'updated_on', 'updated_on', 'content')
-    search_fields = ('name', 'description', 'created_by__username')
-    list_filter = ('created_by', 'created_at',)
+    list_display = ('title', 'author',  'created_on', 'updated_on', 'content')
+    search_fields = ('author', 'title', 'created_on')
+    list_filter = ('author', 'created_on')
 
--- task_notes/models.py
+++ task_notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from dj_tasks.models import UserChecklistStep
 
 # Create your models here.
@@ -6,7 +7,7 @@ from dj_tasks.models import UserChecklistStep
 class Notes(models.Model):
     title = models.CharField(max_length=150)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    template_step = models.ForeignKey(template_step.UserChecklistStep)
+    template_step = models.ForeignKey(UserChecklistStep, on_delete=models.CASCADE, related_name='related_notes')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
