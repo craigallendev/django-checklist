966cd4bc6f8348af2ae5730ccc12d72706044804
Dan Feng <d.feng@hotmail.com>
2024-08-19 12:38:07 +0000

    Link nav bar with checklists

--- check_django/settings.py
+++ check_django/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'dj_tasks.context_processors.navbar_checklist_templates',
             ],
         },
     },
